b75df68ca3baf2f1f9c903ee21ae7c8cf36bf697
lihbr <lihbr@users.noreply.github.com>
2019-08-10 14:46:14 +0200

    :truck: small refactor

---
+++ core/JAMPress.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * JAMPress core class
+ */
+class JAMPress {
+  /**
+   * API response body
+   *
+   * @var array
+   */
+  public static $body = [];
+
+  /**
+   * API response headers
+   *
+   * @var array
+   */
+  public static $headers = [];
+
+  /**
+   * Init class
+   *
+   * @return void
+   */
+  public static function init() {
+    add_action( 'init', 'JAMPress::wpInit' );
+    add_action( 'shutdown', 'JAMPress::wpShutdown' );
+  }
+
+  /**
+   * WordPress init hook action
+   *
+   * @return void
+   */
+  public static function wpInit() {
+    self::$headers[] = "Content-Type: application/json; charset=UTF-8";
+  }
+
+  /**
+   * WordPress shutdown hook action
+   *
+   * @return void
+   */
+  public static function wpShutdown() {
+    self::send();
+  }
+
+  /**
+   * Add JAMPress info to response body
+   *
+   * @return void
+   */
+  private static function addInfo() {
+    JAMPress::$body['jampress'] = [
+      "author" => "edhbr",
+      "git" => "https://github.com/edhbr/misc-wp-jampress",
+      "version" => "0.1.0"
+    ];
+  }
+
+  /**
+   * Set response headers
+   *
+   * @return void
+   */
+  private static function setResHeaders() {
+    foreach (self::$headers as $header) {
+      header($header);
+    }
+  }
+
+  /**
+   * Send API response
+   *
+   * @return void
+   */
+  public static function send() {
+    self::addInfo();
+    self::setResHeaders();
+    $body = json_encode(self::$body);
+    if (!preg_match("/" . base64_decode('amFtcHJlc3M=') . ".*" . base64_decode('ZWRoYnI=') . ".*" . base64_decode('bWlzYy13cC1qYW1wcmVzcw==') . "/", $body)) die("{}");
+    die($body);
+  }
+}
--- jampress.php
+++ jampress.php
@@ -8,33 +8,9 @@
  * Author URI: https://github.com/edhbr
  */
 
-class JAMPress {
-  public static $res = [];
+$jamPress_dir = dirname( __FILE__ );
 
-  public static function init() {
-    add_action( 'init', 'JAMPress::wpInit' );
-    add_action( 'shutdown', 'JAMPress::wpShutdown' );
-  }
-
-  public static function wpInit() {
-    JAMPress::$res['jampress'] = [
-      "author" => "edhbr",
-      "git" => "https://github.com/edhbr/misc-wp-jampress",
-      "version" => "0.1.0"
-    ];
-  }
-
-  public static function wpShutdown() {
-    header("Content-Type: application/json; charset=UTF-8");
-    self::send();
-  }
-
-  public static function send() {
-    $res = json_encode(self::$res);
-    if (!preg_match("/" . base64_decode('amFtcHJlc3M=') . ".*" . base64_decode('ZWRoYnI=') . ".*" . base64_decode('bWlzYy13cC1qYW1wcmVzcw==') . "/", $res)) die("{}");
-    die($res);
-  }
-}
+require( "$jamPress_dir/core/JAMPress.php" );
 
 JAMPress::init(true);
 
